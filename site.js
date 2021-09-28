16315fb1975d8204ab89aa805c526d57defc7a58
Andrew C <83673744+actiniumn404@users.noreply.github.com>
2021-09-28 00:42:02 +0000

    FULL Website Remodel!

--- site.js
+++ site.js
@@ -1,210 +1,189 @@
-dict = {};
 function typetitle(
-    emt,
-    typertext,
-    aftertext,
-    id,
-    total,
-    speed = 50,
-    charPos = 0
+  emt,
+  typertext,
+  aftertext,
+  id,
+  total,
+  speed = 50,
+  charPos = 0
 ) {
-    console.log(id)
-    if (!emt.attr("style")) {
-        emt.attr("style", "");
+  console.log(id)
+  if (!emt.attr("style")) {
+    emt.attr("style", "");
+  }
+  emt.html(" ");
+  let stylesheet = "font-size:18px;font-family:monospace;color:gray !important;transition: 1s ease;"
+  emt.attr("style", emt.attr("style") + stylesheet);
+  i = setInterval(function() {
+    if (charPos >= typertext.length) {
+      if (aftertext === null) {
+        aftertext = typertext;
+      }
+      //setTimeout(() => {
+      emt.html(aftertext);
+      emt.attr(
+        "style",
+        emt.attr("style").substring(0, emt.attr("style").length - stylesheet.length)
+      );
+      //}, 500);
+      clearInterval(id);
+      if (id === 0) {
+        clearInterval(1);
+      }
+      //clearInterval(dict[typertext])
+      return 0;
     }
-    emt.html(" ");
-    emt.attr("style", emt.attr("style") + "font-size:18px;color:#CCC;");
-    i = setInterval(function () {
-        dict[typertext] = i;
-        if (charPos >= typertext.length) {
-            if (aftertext === null) {
-                aftertext = typertext;
-            }
-            //setTimeout(() => {
-                emt.html(aftertext);
-                emt.attr(
-                    "style",
-                    emt.attr("style").substring(0, emt.attr("style").length - 26)
-                );
-            //}, 500);
-            clearInterval(id);
-            if (id === 0) {
-                clearInterval(1);
-            }
-            //clearInterval(dict[typertext])
-            return 0;
-        }
-        emt.append(typertext[charPos]);
-        charPos++;
-    }, speed);
+    emt.append(typertext[charPos]);
+    charPos++;
+  }, parseInt($("#typewriterDuration").val()) / typertext.length);
 }
-counter = 4;
 async function init() {
-    $("#main").css("opacity", "1")
-    for (let x = 0; x < $("#main > section *").length; x++) {
-        el = $($("#main > section *")[x]);
-        if (el.data("code") !== undefined) {
-            counter += 1;
-            console.log(counter)
-            typetitle(
-                el,
-                el.data("code"),
-                el.html(),
-                counter,
-                $("#main > section > *").length
-            );
-        }
+  $("#main").css("opacity", "1")
+	if (anime){
+		for (let x = 0; x < $("#main *").length; x++) {
+			el = $($("#main *")[x]);
+			if (el.data("code") !== undefined) {
+				counter += 1;
+				console.log(counter)
+				typetitle(
+					el,
+					el.data("code"),
+					el.html(),
+					counter,
+					$("#main > section > *").length
+				);
+			}
+		}
+	}
+}
+
+function cursormove() {
+	if (anime){
+		counter = 5
+		setTimeout(() => {
+			$("#cursor").attr("src", "/images/cursor_pointer.png")
+			$("#cursor").attr("width", "22px")
+			setTimeout(() => {
+				$("#runcode").css("filter", "brightness(0.8)")
+				setTimeout(() => {
+					$("#runcode").css("filter", "")
+					setTimeout(() => {
+						$("#cursor").remove()
+						$("#navfiles").trigger("click")
+					}, 500);
+					init()
+				}, 200);
+			}, 500);
+		}, 1500);
+	}else{
+		counter = 0
+		$("#cursor").remove()
+	}
+}
+
+async function o(filename) {
+  //$("main").html("<h1 style='text-align:center'>Loading data, please wait</h1>")
+	window.location.hash = filename
+  $(".file.active").removeClass("active")
+
+  document.querySelectorAll(".file").forEach(e => {
+    if ($($(e).children()[1]).html() === filename) {
+      $(e).addClass("active")
     }
+  })
+
+  let d = await fetch("pages/" + filename)
+  d = await d.text()
+  let hcode = /<main>(.*)<\/main>/ims
+  let style = /<style>(.*)<\/style>/ims
+  let script = /<script>(.*)<\/script>/ims
+
+  hcode = hcode.exec(d)[1]
+
+  if (style.exec(d) !== null) {
+    style = style.exec(d)[1]
+  } else {
+    style = ""
+  }
+
+  if (script.exec(d) !== null) {
+    script = script.exec(d)[1]
+  } else {
+    script = ""
+  }
+
+  $("main").html(hcode)
+  $("#ccss").html(style)
+  $("#ccss").html(style)
+
+  if (notfirst) {
+    init()
+  } else {
+    notfirst = true
+  }
 }
 
-setTimeout(() => {
-    $("#cursor").attr("src", "https://lmddgtfy.net/img/cursor-pointer.png")
-    setTimeout(() => {
-        $("#cursor").css("top", "13px")
-        $("#cursor").css("left", "calc(100vw - 92px)")
-        setTimeout(() => {
-            $("#cursor").css("top", "10px")
-            $("#cursor").css("left", "calc(100vw - 95px)")
-            setTimeout(() => {
-                $("#cursor").remove()
-            }, 200);
-            init()
-        }, 100);
-    }, 500);
-}, 1500);
+let notfirst = false
+let anime = true
+if (getCookie("__animation") === ""){
+	document.cookie = "__animation=true; expires=31 Dec 3000 12:00:00 UTC; path=/";
+}
+if (getCookie("__animation") === "true"){
+	anime = true
+}if (getCookie("__animation") === "false"){
+	$("#main").css("opacity", "1")
+	$("#navfiles").trigger("click")
+	anime = false
+}
+$("#animationChoice").prop("checked", anime)
+changedp()
 
-async function o(filename){
-    data = await fetch(filename)
-    data = data.text()
-    //$("main").html(mainpage[0])
-    //$("main").append(`<style>${mainpage[1]}</style>`)
-    console.log(data)
+cursormove()
+if (location.hash === ""){
+  location.hash = "homepage.html"
+}
+o(location.hash.substring(1))
+
+$(".file").click(e => {
+  x = $(e.target)
+  if (!x.hasClass("file")) {
+    x = $(x.parent())
+  }
+  o($(x.children()[1]).html())
+})
+
+function opennav(id, item){
+	$(".navbarbutton").removeClass("active")
+	$(item).addClass("active")
+	$("nav > div > div").css("display", "none")
+	$("#"+id).css("display", "block")
+}
+// w3schools is awesome
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
-const mainpage = [
-    `			<section style="text-align: center">
-    <h1
-        style="margin-top: 17vh;"
-        data-code="webpage.print('{Hi, I'm Andrew!}', template=big_h1)"
-    >
-        {Hi, I'm Andrew!}
-    </h1>
-    <hr style="width: 200px;" />
-    <h2 data-code="webpage.print('Self-learning coder', template=h2)">
-        Self-learning coder
-    </h2>
-
-    <section>
-        <a
-            href="https://artofproblemsolving.com/community/user/andrewc10"
-            target="_blank"
-            data-code="webpage.linkto('My AoPS Profile')"
-        >
-            <img
-                class="icon"
-                onmouseover="findmeon('AoPS', '@AndrewC10')"
-                src="https://artofproblemsolving.com/online-favicon.ico"
-                alt="AoPS icon"
-            />
-        </a>
-        <a
-            href="https://replit.com/@AndrewChen51/"
-            target="_blank"
-            data-code="webpage.linkto('My Replit Profile')"
-        >
-            <img
-                class="icon"
-                onmouseover="findmeon('Replit', '@AndrewChen51')"
-                src="https://replit.com/public/icons/favicon-196.png"
-                alt="replit icon"
-            />
-        </a>
-        <a
-            href="https://github.com/actiniumn404/"
-            target="_blank"
-            data-code="webpage.linkto('My Github Profile')"
-        >
-            <img
-                class="icon"
-                style="filter: invert(1)"
-                onmouseover="findmeon('Github', '@actiniumn404')"
-                src="https://github.githubassets.com/favicons/favicon.svg"
-                alt="github icon"
-            />
-        </a>
-        <a
-            href="https://discordapp.com/users/728297793646624819"
-            target="_blank"
-            data-code="webpage.linkto('My Discord Profile')"
-        >
-            <img
-                class="icon"
-                onmouseover="findmeon('Discord', '#728297793646624819')"
-                src="https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-logodownload-download-logotipos-1.png"
-                alt="discord icon"
-                height="32px"
-            />
-        </a>
-        <a
-            href="https://stackoverflow.com/users/15947148/actinium404"
-            target="_blank"
-            data-code="webpage.linkto('My stackoverflow Profile')"
-        >
-            <img
-                class="icon"
-                onmouseover="findmeon('Stack Overflow', '@actinium404')"
-                src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196"
-                alt="Stack Overflow icon"
-            />
-        </a>
-        <div id="findmeon">
-            <span
-                style="font-size: 18px;"
-                data-code="webpage.print('Find me on % {}', white, yellow)"
-                >Find me on
-                <span id="sitename" style="color: yellow;"
-                    >{Hover over one of the above!}</span
-                >:</span
-            >
-            <h2
-                id="username"
-                style="
-                    color: yellow;
-                    font-size: 24px;
-                    overflow: auto;
-                    overflow-y: hidden;
-                "
-                data-code="webpage.print('', template=uname, id=uname)"
-            ></h2>
-        </div>
-    </section>
-</section>`,
-`			
-#main h1 {
-    color: #04a0ae;
-    font-size: 72px;
-}
-#main h2 {
-    color: orange;
-    font-size: 36px;
-}
-#main * {
-    transition: 1s font-size ease, 1s color ease;
-}
-
-.icon {
-    width: 32px;
-    height: 32px;
-}
-.icon:hover {
-    opacity: 0.8;
-}`
-]
-
-switch (location.pathname){
-    case "/":
-        $("main").html(mainpage[0])
-        $("main").append(`<style>${mainpage[1]}</style>`)
-        break
-
+function changedp(){
+	if ($("#animationChoice").prop("checked")){
+		anime = true
+		$("#runlog > div").css("opacity", "1")
+		$("#runlog > div").css("pointer-events", "auto")
+		document.cookie = "__animation=true; expires=31 Dec 3000 12:00:00 UTC; path=/";
+	}else{
+		anime = false
+		$("#runlog > div").css("opacity", "0.3")
+		$("#runlog > div").css("pointer-events", "none")
+		document.cookie = "__animation=false; expires=31 Dec 3000 12:00:00 UTC; path=/";
+	}
 }
