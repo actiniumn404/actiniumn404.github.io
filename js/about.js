172beb3cd816436fe1589f227c944ed087809651
Andrew Chen <83673744+actiniumn404@users.noreply.github.com>
2022-02-06 20:30:07 +0000

    contacts

--- js/about.js
+++ js/about.js
@@ -7,7 +7,7 @@ skillset = [
 	{
 		"name":"JavaScript",
 		"image_url":"https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/javascript.svg",
-		"description":"",
+		"description":"JavaScript (JS) ladies and gentlemen, is what makes webpages awesome. It could even be argued that it is the most important of the web development languages. I learned JS in December of 2020, and since then, I have become pretty good at it",
 	},
 	{
 		"name":"HTML",
@@ -17,12 +17,12 @@ skillset = [
 	{
 		"name":"CSS",
 		"image_url":"https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/css.svg",
-		"description":"",
+		"description":`CSS is what makes websites look good. Without CSS, webpages would look horrible. How horrible? Well click <button onclick="document.querySelectorAll('link').forEach(x=>x.remove())">this button</button> to find out. Anyways, CSS was the first web development language I learned, which might be a bit odd to you, as most people learn HTML first.`,
 	},
 	{
 		"name":"LaTeX",
 		"image_url":"https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/tex.svg",
-		"description":"",
+		"description":"LaTeX",
 	},
 	{
 		"name":"NodeJS",
